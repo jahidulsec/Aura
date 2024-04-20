97413eb519e82c0b9cc9daf03830e0d7bc560b57
Jahidul Islam <jahidul.islam.sec@gmail.com>
2024-04-20 22:29:59 +0600

    create bookmark table and fetch data

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -2,14 +2,16 @@ import { View, Text, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SearchInput from "../../components/SearchInput";
 import EmptyState from "../../components/EmptyState";
-import { searchPosts } from "../../lib/appwrite";
+import { getUserSavedPosts, searchPosts } from "../../lib/appwrite";
 import useAppwrite from "../../hooks/useAppwrite";
 import VideoCard from "../../components/VideoCard";
 import { useState } from "react";
+import { useGlobalContext } from "../../context/GlobalProvider";
 
 const Bookmark = () => {
   const [query, setQuery] = useState("");
-  const { data: posts } = useAppwrite(() => searchPosts(query));
+  const {user} = useGlobalContext()
+  const { data: posts } = useAppwrite(() => getUserSavedPosts(user?.$id));
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -26,6 +28,7 @@ const Bookmark = () => {
             <SearchInput
               placeholder="Search your saved videos"
               bookmarkSearch={true}
+              value={query}
               handleChangeText={(e) => {setQuery(e)}}
             />
           </View>
